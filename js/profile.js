0acc904e3b1ca7c7c2ad84ae2916c79bd84127b7
Kaveesha Weerasiri <kweera2@uic.edu>
2020-04-26 23:59:03 +0000

    activity_13.3

--- js/profile.js
+++ js/profile.js
@@ -3,10 +3,15 @@ let profile = {
   lastName: "Weerasiri",
   //add projects by adding more objects
   projects: [
-    {name: "Display data about the coronavirus w",
+    {name: "Cornovirus Chart",
      technologies: "HTML, CSS, JS, api from https://pomber.github.io/covid19/timeseries.json",
      description: "A project that plots coronavirus data",
      projectURL: "https://kaveeshaw.github.io/it202-project4/",
-     repoURL: "https://github.com/KaveeshaW/it202-project4"}
+     repoURL: "https://github.com/KaveeshaW/it202-project4"},
+    {name: "Side Scrolling Game",
+     technologies: "HTML, CSS, JS, Canvas",
+     description: "Tom and Jerry inspired",
+     projectURL: "https://kaveeshaw.github.io/it202-project3/",
+     repoURL: "https://github.com/KaveeshaW/it202-project3"}
    ]
 }
