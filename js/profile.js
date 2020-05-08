06f6c30c7714fc1255439b080332032b2c3f327e
Kaveesha Weerasiri <kweera2@uic.edu>
2020-05-08 18:17:23 +0000

    changed what information is being stored

--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,32 @@
 let profile = {
   firstName: "Kaveesha",
   lastName: "Weerasiri",
+  experience: [
+    {
+      name: "Coding Languages",
+      pr: ""
+    }
+  ],
   //add projects by adding more objects
   projects: [
     {name: "Cornovirus Chart",
      technologies: "HTML, CSS, JS, api from https://pomber.github.io/covid19/timeseries.json",
      description: "A project that plots coronavirus data",
      projectURL: "https://kaveeshaw.github.io/it202-project4/",
-     repoURL: "https://github.com/KaveeshaW/it202-project4"},
+     repoURL: "https://github.com/KaveeshaW/it202-project4",
+     date: "March 2020 to April 2020"},
     {name: "Side Scrolling Game",
      technologies: "HTML, CSS, JS, Canvas",
      description: "Tom and Jerry inspired",
      projectURL: "https://kaveeshaw.github.io/it202-project3/",
-     repoURL: "https://github.com/KaveeshaW/it202-project3"}
+     repoURL: "https://github.com/KaveeshaW/it202-project3",
+     date: "March 2020"},
+    {name: "Invaders Of The Void",
+     technologies: "C#, Unity",
+     description: "Player traverses through space, then a castle and faces different puzzle challenges as well as a boss",
+     projectURL: "not defined",
+     repoURL: "https://github.com/KaveeshaW/InvadersOfTheVoid",
+     date: "Feburary 2020 to April 2020"
+    }
    ]
 }
